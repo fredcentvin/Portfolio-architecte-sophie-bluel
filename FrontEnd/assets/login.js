50687910ed1e75ef547b5c9be319673a5f68c751
fred centvin <fbplayback1@gmail.com>
2024-05-20 16:12:37 +0200

    page login fonctionnelle

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,43 +1,38 @@
 //**constantes globales */
 const apiUrl = "http://localhost:5678/api/";
-const email = document.querySelector("form #mail");
-const password = document.querySelector("form #password");
+let userInfos ={
+email : document.getElementById("email"),
+password : document.getElementById("password")
+};
 const form = document.querySelector("form");
 const submit = document.querySelector("form #submit");
 const messErreur = document.querySelector("#connexion p");
-
+console.log(userInfos)
 
 //**ecouteur sur le bouton envoyer */
 submit.addEventListener("click", (e) => {
-    e.preventDefault()
-    const email = e.target.email;
-    const password = e.target.password;
+    e.preventDefault();
     login();
 })
 
 //**envoi identifiants de connexion */
 async function login() {
     try{
-        const response = fetch(apiUrl+"users/login",{
+        const response = await fetch(apiUrl+"users/login",{
             method: "POST",
-            headers: {"content-type":"application/json"},
-            body: JSON.stringify({email, password})
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({email:userInfos.email.value, password:userInfos.password.value}),
         })
         if (response.ok) {
             const data = await response.json();
             localStorage.setItem("token", data.token);
-            window.location.href = "./index.html"
+            window.location.href = ".././index.html";
         } else {
             console.error("Erreur lors de la connexion à l'API");
+            messErreur.textContent = "erreur lors de la connexion, veuillez reessayer";
         }
     } catch (error) {
         console.error("Erreur réseau :", error);
+        messErreur.textContent = "Veuiiez vérifier votre connexion";
     }
     }
-   
-
-
-
-
-
-
